d862e34ea2dfd28f5ecca2310d1265de991191ff
Dasom Hwang <moa8417@naver.com>
2022-12-22 12:23:47 +0900

    20221222 test1

---
+++ datori2/src/main/java/com/example/Datori2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datori2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Datori2Application.class, args);
+	}
+
+}
